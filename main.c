633e28b1974643a9380c15a803ca7430ec0c3953
Legend <me@liux.pro>
2026-09-22 20:12:00 +0800

    引入h

--- main.c
+++ main.c
@@ -6,7 +6,14 @@
 #include <string.h>
 
 
+#define HAS_FLOAT 0
+#define HAS_TIME_H 0
+#define USE_CLOCK 0
+#define HAS_STDIO 1
+#define HAS_PRINTF 1
 
+
+#include "coremark.h"
 
 /**
  * 检查是否接收到特定的ISP固件升级请求。
